5182e4c2378fc87a9d55dc32275e444ae3e776a0
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-11-30 10:20:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,85 @@
-window.addEventListener("load",addlistener); 
-var Number_of_things,Things,count = 0,inputter;
-let lstThings = [];
-function addlistener()
+window.addEventListener("load",Getthings); 
+let ListAnswers = [];
+function Getthings()
 {
-	document.getElementById("Output").style.display = "none";
-	document.getElementById("btn1").addEventListener("click",Getelements);
-	document.getElementById("btn2").addEventListener("click",GetThings);
+    document.getElementById("Answers").style.display = "none";
+    document.getElementById("Question2").style.display = "none"; 
+    document.getElementById("Question3").style.display = "none"; 
+    document.getElementById("BlueWhale").addEventListener("click",Correct);
+    document.getElementById("Lion").addEventListener("click",Wrong);
+    document.getElementById("African").addEventListener("click",Wrong);
+    document.getElementById("American").addEventListener("click",Wrong);
+    document.getElementById("Trump").addEventListener("click",Wrong2);
+    document.getElementById("Mao").addEventListener("click",Wrong2);
+    document.getElementById("Xijingping").addEventListener("click",Correct2);
+    document.getElementById("DengXiaoPing").addEventListener("click",Wrong2);
+    document.getElementById("SolutionInput").addEventListener("click",Check);
+
 
 }
-
-function Getelements()
+function Correct()
 {
-	Number_of_things = document.getElementById("txtHowmany").value; 
-	var check = Number_of_things % 1; 
-	
-	if(check != 0 || Number_of_things == "")
-	{
-		alert("Type in a correct input!");
-		document.getElementById("txtHowmany").value = ""; 
-		document.getElementById("txtHowmany").focus();
-	}
-	else 
-	{
-		document.getElementById("txtThings").focus();
-	}
-	
+    document.getElementById("Question2").style.display = "block";
+    document.getElementById("Question1").style.display = "none"; 
+    document.getElementById("Solution1").focus();
+    ListAnswers.push("Correct");
 }
-function GetThings()
+function Wrong()
 {
-	Things = document.getElementById("txtThings").value; 
-	if(Things == "")
-	{
-		alert("Type in a correct input!");
-		document.getElementById("txtThings").value = ""; 
-		document.getElementById("txtThings").focus();
-	}
-	else 
-	{
-		lstThings.push(Things);
-		document.getElementById("txtThings").focus();
-		document.getElementById("txtThings").value = "";
-		count = count + 1;
-	}
-	
-	if(count == parseInt(Number_of_things))
-	{
-		let Todo = document.getElementById("TodoList");
-		let length = lstThings.length;
-		for(let i = 0; i < length; i++)
-		{
-			let li = document.createElement("li"); 
-			li.innerText = lstThings[i]; 
-			Todo.appendChild(li);
-		}
-		document.getElementById("Inputs").style.display = "none";
-		document.getElementById("Output").style.display = "block";
-	}
+    document.getElementById("Question2").style.display = "block";
+    document.getElementById("Question1").style.display = "none"; 
+    document.getElementById("Solution1").focus();
+    ListAnswers.push("Incorrect");
+}
+function Check()
+{
+    var Value1 = document.getElementById("Solution1").value; 
+    var value2 = document.getElementById("Solution2").value; 
+    let check = !isNaN(Value1); 
+    let check2 = !isNaN(value2);
+    if(Value1 == "" || value2 == "" || check == false || check2 == false)
+    {
+        document.getElementById("Solution1").value = ""; 
+        document.getElementById("Solution2").value = ""; 
+        document.getElementById("Solution1").focus();
+        alert("Type in a correct value")
+    }
+    else
+    {
+        sum = parseInt(Value1) + parseInt(value2);
+        if(sum == 5 )
+        {
+            ListAnswers.push("Correct");
+        }
+        else
+        {
+            ListAnswers.push("Incorrect");
+        }
+        document.getElementById("Question2").style.display = "none";
+        document.getElementById("Question3").style.display = "block";
+    }
+}
+function Correct2()
+{
+    ListAnswers.push("Correct");
+    Answers()
+}
+function Wrong2()
+{
+    ListAnswers.push("Incorrect");
+    Answers()
+}
+function Answers()
+{
+    let length = ListAnswers.length;
+    let element = document.getElementById("Correct/Wrong");
+
+    for (let i = 0; i < length; i++)
+    {
+        let li = document.createElement("li"); 
+        li.innerText = ListAnswers[i]; 
+        element.appendChild(li);
+    }
+    document.getElementById("Question3").style.display = "none"; 
+    document.getElementById("Answers").style.display = "block";
 }

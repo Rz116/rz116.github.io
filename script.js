23848254fad990cf15413499936093767d2c5375
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-12-19 18:10:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,138 +1,29 @@
-window.addEventListener("load",Getthings); 
-let ListAnswers = [];
-var count = 0;
-function Getthings()
+window.addEventListener("load",direct); 
+function direct()
 {
-    document.getElementById("Answers").style.display = "none";
-    document.getElementById("Question2").style.display = "none"; 
-    document.getElementById("Question3").style.display = "none"; 
-    document.getElementById("Question4").style.display = "none";
-    document.getElementById("Question5").style.display = "none";
-    document.getElementById("BlueWhale").addEventListener("click",Correct);
-    document.getElementById("Lion").addEventListener("click",Wrong);
-    document.getElementById("African").addEventListener("click",Wrong);
-    document.getElementById("American").addEventListener("click",Wrong);
-    document.getElementById("Trump").addEventListener("click",Wrong2);
-    document.getElementById("Mao").addEventListener("click",Wrong2);
-    document.getElementById("Xijingping").addEventListener("click",Correct2);
-    document.getElementById("DengXiaoPing").addEventListener("click",Wrong2);
-    document.getElementById("SolutionInput").addEventListener("click",Check);
-    document.getElementById("Input").addEventListener("click",check2);
-    document.getElementById("Answer1").addEventListener("click",Correct3);
-    document.getElementById("Answer2").addEventListener("click",Wrong3);
-    document.getElementById("Answer3").addEventListener("click",Wrong3);
-    document.getElementById("Answer4").addEventListener("click",Wrong3);
+    document.getElementById("benefit1").addEventListener("click",Direct1);
+    document.getElementById("benefit2").addEventListener("click",Direct2);
+    document.getElementById("consequence1").addEventListener("click",Direct4);
+    document.getElementById("consequence2").addEventListener("click",Direct5);
+    document.getElementById("BM").addEventListener("click",Direct3);
 }
-function Correct()
+function Direct1()
 {
-    document.getElementById("Question2").style.display = "block";
-    document.getElementById("Question1").style.display = "none"; 
-    document.getElementById("Solution1").focus();
-    ListAnswers.push("Correct");
+    window.location.href = "benefit1.html";
 }
-function Wrong()
+function Direct2()
 {
-    document.getElementById("Question2").style.display = "block";
-    document.getElementById("Question1").style.display = "none"; 
-    document.getElementById("Solution1").focus();
-    ListAnswers.push("Incorrect");
+    window.location.href = "benefit2.html";
 }
-function Check()
+function Direct3()
 {
-    var Value1 = document.getElementById("Solution1").value; 
-    var value2 = document.getElementById("Solution2").value; 
-    let check = !isNaN(Value1); 
-    let check2 = !isNaN(value2);
-    if(Value1 == "" || value2 == "" || check == false || check2 == false)
-    {
-        document.getElementById("Solution1").value = ""; 
-        document.getElementById("Solution2").value = ""; 
-        document.getElementById("Solution1").focus();
-        alert("Type in a correct value")
-    }
-    else
-    {
-        if(Value1 == 2 && value2 == 3 || Value1 == 3 && value2 == 2)
-        {
-            ListAnswers.push("Correct");
-        }
-        else
-        {
-            ListAnswers.push("Incorrect");
-        }
-        document.getElementById("Question2").style.display = "none";
-        document.getElementById("Question3").style.display = "block";
-    }
+    window.location.href = "Home.html"
 }
-function Correct2()
+function Direct4()
 {
-    ListAnswers.push("Correct");
-    document.getElementById("Question3").style.display = "none"; 
-    document.getElementById("Question4").style.display = "block";
-    document.getElementById("solution").focus();
+    window.location.href = "consequence1.html";
 }
-function Wrong2()
+function Direct5()
 {
-    ListAnswers.push("Incorrect");
-    document.getElementById("Question3").style.display = "none"; 
-    document.getElementById("Question4").style.display = "block";
-    document.getElementById("solution").focus();
-}
-function check2()
-{
-    var value = document.getElementById("solution").value; 
-    var checking = !isNaN(value); 
-    if(value == "" || checking == false)
-    {
-        alert("Type in a valid input(HAS TO BE A NUMBER)")
-        document.getElementById("solution").value = ""; 
-        document.getElementById("solution").focus();
-    }
-    else 
-    {
-        if(value == 4 || value == 2)
-        {
-            ListAnswers.push("Correct");
-        }
-        else
-        {
-            ListAnswers.push("Incorrect"); 
-        }
-        document.getElementById("Question4").style.display = "none";
-        document.getElementById("Question5").style.display = "block";
-    }
-}
-function Correct3()
-{
-    ListAnswers.push("Correct");
-    Answers()
-}
-function Wrong3()
-{
-    ListAnswers.push("Incorrect"); 
-    Answers()
-}
-function Answers()
-{
-    let length = ListAnswers.length;
-    let element = document.getElementById("Correct/Wrong");
-
-    for (let i = 0; i < length; i++)
-    {
-        let li = document.createElement("li"); 
-        li.innerText = ListAnswers[i]; 
-        element.appendChild(li);
-    }
-
-    let removeitem = "Correct"; 
-    let wrong = ListAnswers.filter(value => value !=removeitem); 
-    let length2 = wrong.length; 
-    let difference = length - length2; 
-    let percent = difference/length;
-    let other = percent * 100;
-    let rounded = Math.round(other);
-    
-    document.getElementById("percentage").textContent =  rounded + "%";
-    document.getElementById("Question5").style.display = "none"; 
-    document.getElementById("Answers").style.display = "block";
+    window.location.href = "consequence2.html";
 }
